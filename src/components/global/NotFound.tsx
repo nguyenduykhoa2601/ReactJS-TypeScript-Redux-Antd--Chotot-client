85c49b0de9ef4db300a78ad0577ff6571ffd01a8
Khoa Nguyen <nguyenduykhoa2601@gmail.com>
2021-11-22 16:38:50 +0700

    set-up project

---
+++ src/components/global/NotFound.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const NotFound = () => {
+    return (
+        <div>
+            NOTFOUND
+        </div>
+    )
+}
+
+export default NotFound
---
+++ src/utils/TypeScript.ts
@@ -0,0 +1,38 @@
+import { ChangeEvent, FormEvent } from "react";
+
+export type InputChange = ChangeEvent<
+	HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+>;
+
+export type FormSubmit = FormEvent<HTMLFormElement>;
+
+export interface IParams {
+	page: string
+	slug: string
+}
+
+export interface IUserLogin {
+	account: string;
+	password: string;
+}
+
+export interface IUserRegister extends IUserLogin {
+	name: string;
+	cf_password: string;
+}
+
+export interface IUser extends IUserLogin {
+	avatar: string;
+	name: string;
+	role: string;
+	type: string;
+	updatedAt: string;
+	createdAt: string;
+	_id: string;
+}
+
+export interface IAlert {
+	success?: string | string[];
+	errors?: string | string[];
+	loading?: boolean;
+}
